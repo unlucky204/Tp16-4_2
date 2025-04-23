bdadba45ea60be6b94b9eb6e340a74830cc23608
unlucky204 <lautaroparrondo@gmail.com>
2025-04-23 20:16:52 -0300

    Add files via upload
    Proyecto terminado y funcional

--- Tp16-4_2/Tp16-4_2/Personaje.cs
+++ Tp16-4_2/Tp16-4_2/Personaje.cs
@@ -6,32 +6,33 @@ using System.Threading.Tasks;
 
 namespace Tp16_4_2
 {
-    internal class Personaje
+    public class Personaje
     {
+        //public Personaje(string color, int vida, int defensa, int fuerza, 
+        //    int mana)
+        //{
+        //    Color = color;
+        //    Vida = vida;
+        //    Defensa = defensa;
+        //    Fuerza = fuerza;
+        //    Mana = mana;
+        //    Dmg = false;
+        //}
+
         public string Color { get; set; }
         public int Vida { get; set; }
         public int Defensa { get; set; }
         public int Fuerza { get; set; }
         public int Mana { get; set; }
+        public bool Dmg { get; set; }
 
-        public int DañoHecho { get; set; }
-
-        public void Cargar ()
-        {
-            Console.WriteLine("ingrese su color");
-            Color = Console.ReadLine();
-            Console.WriteLine("ingrese su vida");
-            Vida = int.Parse(Console.ReadLine());
-            Console.WriteLine("ingrese su defensa");
-            Defensa = int.Parse(Console.ReadLine());
-            Console.WriteLine("ingrese su fuerza");
-            Fuerza = int.Parse(Console.ReadLine());
-            Console.WriteLine("ingrese su mana");
-            Mana = int.Parse(Console.ReadLine());
-        }
+       
         public void CambiarColor(string nuevoColor)
         { 
-            Color = nuevoColor;
+            if (nuevoColor != Color)
+            {
+                Color = nuevoColor;
+            }
         }
 
         public int RecibirDaño(int daño)
@@ -45,7 +46,7 @@ namespace Tp16_4_2
             Vida = Vida - dañoRecibido;
             return dañoRecibido;
         }
-        public int Atacar(Personaje objetivo)
+        public bool Atacar(Personaje objetivo)
         {
 
             if (Mana >= 0)
@@ -53,21 +54,14 @@ namespace Tp16_4_2
                 int daño = Fuerza;
                 Mana--;
                 objetivo.RecibirDaño(daño);
+                Dmg = true;
             }
             else
             {
-                DañoHecho = 0;
+                Dmg = false;
             }
-            return DañoHecho;
+            return Dmg;
         }
-        public void muestra() 
-        {
-            Console.WriteLine($"el color es{Color}");
-            Console.WriteLine($"la vida es{Vida}");
-            Console.WriteLine($"el defensa es{Defensa}");
-            Console.WriteLine($"el Fuerza es{Fuerza}");
-            Console.WriteLine($"el Mana es{Mana}");
-            Console.WriteLine($"el color es{Color}");
-        }
+      
     }
 }
--- Tp16-4_2/Tp16-4_2/Program.cs
+++ Tp16-4_2/Tp16-4_2/Program.cs
@@ -1,1 +1,79 @@
-﻿
+﻿using Tp16_4_2;
+Personaje personaje = new Personaje();
+Console.WriteLine("Bienvenido");
+Cargar(personaje);
+Personaje personaje2 = new Personaje();
+Cargar(personaje2);
+Console.Clear();
+while (true)
+{
+    Muestra(personaje);
+    Muestra(personaje2);
+    menu();
+}
+
+void menu()
+{
+    char op;
+    Console.WriteLine("=====================================");
+    Console.WriteLine("ingrese que accion quiere hacer");
+    Console.WriteLine("1) Cambiar color");
+    Console.WriteLine("2) Recibir daño");
+    Console.WriteLine("3) Atacar");
+    op = Char.Parse(Console.ReadLine());
+    switch (op) 
+    { 
+        case'1':
+            Console.WriteLine("=====================================");
+            Console.WriteLine("ingrese nuevo color");
+            string nuevoColor = Console.ReadLine();
+            personaje.CambiarColor(nuevoColor);
+            break;
+        case '2':
+            Console.WriteLine("=====================================");
+            Console.WriteLine("ingrese el daño que va a recibir");
+            int Daño = int.Parse(Console.ReadLine());
+            personaje.RecibirDaño(Daño);
+            break;
+        case '3':
+            Console.WriteLine("=====================================");
+            Console.WriteLine("ingrese el a que pesonaje quiere atacar");
+            Console.WriteLine("1) Objetivo");
+            personaje.Atacar(personaje2);
+            break;
+    }
+}
+void Cargar(Personaje personaje)
+{
+    Personaje Jugador = new Personaje();
+    Jugador = personaje;
+    Console.WriteLine("=====================================");
+    Console.WriteLine("Ingrese los datos del jugador");
+    Console.WriteLine("ingrese su color");
+    Jugador.Color = Console.ReadLine();
+    Console.WriteLine("ingrese su vida");
+    Jugador.Vida = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese su defensa");
+    Jugador.Defensa = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese su fuerza");
+    Jugador.Fuerza = int.Parse(Console.ReadLine());
+    Console.WriteLine("ingrese su mana");
+    Jugador.Mana = int.Parse(Console.ReadLine());
+
+}
+
+
+void Muestra(Personaje personaje)
+{
+    Personaje Jugador = new Personaje();
+    Jugador = personaje;
+    Console.WriteLine($"==========================================");
+    Console.WriteLine($"el color es {Jugador.Color}");
+    Console.WriteLine($"la vida es {Jugador.Vida}");
+    Console.WriteLine($"el defensa es {Jugador.Defensa}");
+    Console.WriteLine($"el Fuerza es {Jugador.Fuerza}");
+    Console.WriteLine($"el Mana es {Jugador.Mana}");
+    Console.WriteLine($"el color es {Jugador.Color}");
+}
+
+
